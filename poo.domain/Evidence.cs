1dbbdeeab3d80c763014f9c35e49e5472749068d
totaldis <totaldis@gmail.com>
2014-01-27 23:38:16 -0500

    Added poo.data
    Added new project for the data layer and referenced entity framework 6
    in project

--- poo.domain/Evidence.cs
+++ poo.domain/Evidence.cs
@@ -9,11 +9,16 @@ namespace poo.domain
     public abstract class Evidence
     {
         public int Id { get; set; }
-        public int LabId { get; set; }
         public string Name { get; set; } // name of evidence
         public string Description { get; set; } // description of evidence
         public string ImageURL { get; set; } // location of image
         public Priority Importance { get; set; } // importance level
         public DateTime? DateAdded { get; set; } // date uploaded
+
+        public int ResearcherId { get; set; } 
+        public int LabId { get; set; }
+
+        public virtual Researcher Researcher { get; set; }
+        public virtual Lab Lab { get; set; }
     }
 }
--- poo.domain/Researcher.cs
+++ poo.domain/Researcher.cs
@@ -8,32 +8,40 @@ namespace poo.domain
 {
     public class Researcher
     {
+        private ICollection<Photo> _photos;
+        private ICollection<Document> _documents;
+
+        public Researcher()
+        {
+            _photos = new List<Photo>();
+            _documents = new List<Document>();
+        }
+
         public int Id { get; set; } // id of researcher
         public string UserName { get; set; } // user name of researcher
         public int? Age { get; set; } // age of researcher
         public string ImageURL { get; set; } // location of image
-        public virtual ICollection<Photo> EvidencePhoto { get; set; } // collection of photos produced by researcher
-        public virtual ICollection<Document> EvidenceDocument { get; set; } // collection of documents produced by researcher
+
+        public virtual ICollection<Photo> Photos
+        {
+            get { return _photos; }
+            set { _photos = value; }
+        }
+
+        public virtual ICollection<Document> Documents
+        {
+            get { return _documents; }
+            set { _documents = value; }
+        }
 
         public int PhotoCount
         {
-            get
-            {
-                if (EvidencePhoto != null)
-                    return EvidencePhoto.Count;
-                else
-                    return 0;
-            }
+            get { return _photos.Count; }
         }
+
         public int DocumentCount
         {
-            get
-            {
-                if (EvidenceDocument != null)
-                    return EvidenceDocument.Count;
-                else
-                    return 0;
-            }
+            get { return _documents.Count; }
         }
     }
 }
